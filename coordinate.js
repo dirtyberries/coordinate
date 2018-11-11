db1a501fd288a002c1a44d6cd71be3e533827b7c
sharonhuh <huh.sharon@gmail.com>
2018-11-10 21:42:22 -0700

    Update coordinate.js
    Fixed comment spacing

--- coordinate.js
+++ coordinate.js
@@ -4,9 +4,9 @@ const X = 0;
 const Y = 0;
 
 /* coordinate: (1) Takes (x,y) coordinate of a point; (2) If point is inside or
-							 on circle, returns coordinate as is; otherwise, returns
-							 corresponding coordinate on circle; (3) Returns array with
-							 x-coordinate at index 0 and y-coordinate at index 1 */
+		on circle, returns coordinate as is; otherwise, returns
+		corresponding coordinate on circle; (3) Returns array with
+		x-coordinate at index 0 and y-coordinate at index 1 */
 function coordinate(x, y) {
 	// r = distance from point to center of circle
 	const r = Math.sqrt(Math.abs((x - X) ** 2 + (y - Y) ** 2));
